18ce1d3550ece5ddd716412a579859c6840a9891
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-07-23 23:22:41 +0530

    started sample apps

---
+++ About me/About me/About_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_meApp.swift
+//  About me
+//
+//  Created by shashwat singh on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct About_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
